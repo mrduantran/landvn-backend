c44ca86e527b13e66025aa504f2d90b5b7f6bd20
Duan <mrduantran@gmail.com>
2026-05-17 19:07:56 +0700

    update PROD
    Cross
    
    Co-Authored-By: Duan <tdduan@gmail.com>

--- src/main/java/com/landvn/backend/config/SecurityConfig.java
+++ src/main/java/com/landvn/backend/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://landvn-frontend.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
